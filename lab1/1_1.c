5ca4f0f45e7b5af5d5c8b8a1474a85ed95fa5db2
Segoga <129649003+seg0ga@users.noreply.github.com>
2024-09-24 19:19:54 +0700

    Create 1_1.c

---
+++ lab1/1_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+        int a;
+        printf("введите год:");
+        scanf("%d",&a);
+        if (a%400 == 0){
+                printf("Весокосный");
+        } else if (a%100 == 0){
+                printf("Не весокосный");
+        } else if (a%4 == 0){
+                printf("Весокосный");
+        } else{
+                printf("Не весокосный");
+        }
+
+}
