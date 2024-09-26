8ec4b3e41c13c098a5b186a4dbbe2075a546996e
Segoga <129649003+seg0ga@users.noreply.github.com>
2024-09-26 20:11:39 +0700

    Update 4_1.c

--- lab4/4_1.c
+++ lab4/4_1.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main(){
+        int a,k,m;
+        printf("Введите число N: ");
+        scanf("%d",&a);
+        k = 1;
+        for (int i = 0; i<a;i++){
+                m=0;
+                while (m < a){
+                    if (a>3 && k<10)
+                        printf(" ");
+                    if (a>9 && k<100)
+                        printf(" ");
+                    printf("%d ",k);k++;m++;
+                }
+                printf("\n");
+        }
+}
