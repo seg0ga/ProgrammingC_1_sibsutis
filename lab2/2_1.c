24d4b3f083b24069f9cdd53de8f828734483f05f
Segoga <129649003+seg0ga@users.noreply.github.com>
2024-09-26 10:27:14 +0700

    Create 2_1.c

---
+++ lab2/2_1.c
@@ -0,0 +1,16 @@
+//Вариант 6
+#include <stdio.h>
+
+int main(){
+        int a;
+        printf("Введите число: ");
+        scanf("%d",&a);
+        for (int s=0,n=2; n<a; n++){
+                if (a%n==0){
+                        s=1;
+                        printf("Наименьший делитель: %d",n>
+                        break;
+                }
+        }
+
+}
