7a2240ee3a3786656befc1746ea9a28819cbfb62
Segoga <129649003+seg0ga@users.noreply.github.com>
2024-09-26 20:13:33 +0700

    Create 3_3.c

---
+++ lab3/3_3.c
@@ -0,0 +1,12 @@
+//Вариант 8
+#include <stdio.h>
+
+int main(){
+        int n;
+        printf("Введите число: ");
+        scanf("%d",&n);
+        for (int s=1, i=1; i<=n; i++) {
+                s = s * i;
+                printf("%d \n",s);
+        }
+}
