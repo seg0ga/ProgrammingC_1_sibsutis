3c78ab17468e81d6cc0ae1c204eecfce523b46ba
Segoga <129649003+seg0ga@users.noreply.github.com>
2024-10-05 19:41:04 +0700

    Update 5_1.c

--- lab5/5_1.c
+++ lab5/5_1.c
@@ -2,15 +2,22 @@
 #include <stdio.h>
 
 int main(){
-    int digit, count;
+    unsigned int digit, count;
     char digit_str;
-    printf("Введите целое цисло A: ");
+    int flag;
+    printf("Введите 8 битное целое цисло A: ");
     scanf("%d", &digit);
-    printf("Введите количество сдвигов: ");
-    scanf("%d", &count);
-    printf("%d \n", digit);
-    for (int i=0; i<count; i++){
-        digit = digit>>1;
-        printf("%d \n", digit);
+    if (digit<=255 && digit>=0) {
+        printf("Введите количество сдвигов: ");
+        scanf("%d", &count);
+        for (int i=0; i<count; i++){
+            flag = 0;
+            if (digit %2 ==1)
+                flag = 1;
+            digit = digit>>1;
+            if (flag==1)
+                digit= digit+128;
+            printf("%d \n", digit);}
+    }else
+        printf("Неправильное число");
     }
-}
