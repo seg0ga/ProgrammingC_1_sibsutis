04a2f5e7bfd677c7614b8d9d36e774456f7eec6c
Segoga <129649003+seg0ga@users.noreply.github.com>
2024-10-10 10:29:24 +0700

    Update 5_1(pro version).c

--- lab5/5_1(pro version).c
+++ lab5/5_1(pro version).c
@@ -20,7 +20,7 @@ int main(){
         scanf("%d", &count);
         for (int i=0; i<count; i++){
             flag = 0;
-            if (digit %2 ==1)
+            if (digit&1==1)
                 flag = 1;
             digit = digit>>1;
             if (flag==1)
