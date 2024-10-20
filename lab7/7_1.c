e77bbffbe063a83659aa7387a5f357e02a78e624
Segoga <129649003+seg0ga@users.noreply.github.com>
2024-10-20 14:47:30 +0700

    Update 7_1.c

--- lab7/7_1.c
+++ lab7/7_1.c
@@ -1,22 +1,24 @@
 //Вариант 14 (На оценку "5")
-#include <stdio.h>
+#include <stdio.h> 
 #include <string.h>
 #include <ctype.h>
 
 int main() {
     char predlog[100];
     int sum = 0;
+    
+    //Запрашиваем и считываем предложение
     printf("Введите предложение: ");
     fgets(predlog, 100, stdin);
+
+    //Идем по строке и смотрим на каждый символ на предмет того, что это число
     for (int i = 1; i < 100; i++) {
         if (isdigit(predlog[i]))
-            sum += predlog[i] - '0';
-    }
-    if (sum > 0) {
+            sum += predlog[i] - '0';}
+    
+    //смотрим есть ли числа и если есть то выводим их сумму
+    if (sum > 0)
         printf("Сумма: %d\n", sum);
-    } else {
+    else
         printf("Цифр нет\n");
-    }
-
-    return 0;
 }
