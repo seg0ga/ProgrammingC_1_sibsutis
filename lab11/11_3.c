d0296df322f44fe7991c02c8b3e426f7830e417e
Segoga <129649003+seg0ga@users.noreply.github.com>
2024-12-05 10:36:05 +0700

    Update 11_3.c

--- lab11/11_3.c
+++ lab11/11_3.c
@@ -1,9 +1,8 @@
 #include <stdio.h>  
   
-#define SQR(x)  (x)*(x)
+#define POW(x)(x)*(x)  
 int main() {      
-int y = 5;      
-int z = SQR(y + 1);      
-printf("z = %d\n", z);      
-return 0;
-}  
+    int y = 5;      
+    int z = POW(y + 1);      
+    printf("z = %d\n", z);      
+    return 0;} 
