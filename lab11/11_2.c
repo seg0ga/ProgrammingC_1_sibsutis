b81e1dbf1088b96277155e62b637684d6095e6cf
Segoga <129649003+seg0ga@users.noreply.github.com>
2024-12-05 10:28:32 +0700

    Create 11_2.c

---
+++ lab11/11_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>  
+  
+typedef struct{  
+    char str[10];      
+    int num;  
+} NumberRepr;  
+  
+void format(NumberRepr *number){      
+    sprintf(number->str, "%3d", number->num);}   
+int main(){  
+    NumberRepr number = { .num = 1025 };  
+    format(&number);  
+    printf("str: %s\n", number.str);      
+    printf("num: %d\n", number.num);  
+    return 0;}
